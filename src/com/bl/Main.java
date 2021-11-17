65a5e2edbc7e73b1ec0cc0d0d535fc4cc942049d
unknown <akshayHVshinde04@gmail.com>
2021-11-17 23:11:27 +0530

    Added: UC3_EditContacts

--- src/com/bl/Main.java
+++ src/com/bl/Main.java
@@ -4,35 +4,38 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main {
-	public static String userInput() {
-		Scanner scan = new Scanner(System.in);
-		String string = scan.nextLine();
-		return string;
+	static ArrayList<PersonalDetails> contact = new ArrayList<PersonalDetails>();
+	public static Scanner scanner = new Scanner(System.in);
+	static int menu;
+	public static int choice() {
+		System.out.println(" 0. Exit. ");
+		System.out.println(" 1. Edit contact. ");
+		menu = scanner.nextInt();
+		return menu;
 	}
 	public static void main(String[] args) {
-		System.out.println("Welcome to Address Book Program");
-	  
-        ArrayList<String> addressList = new ArrayList<>();
-		
-		PersonalDetails personOne = new PersonalDetails();
-		System.out.println("Please Enter the Name :");
-		personOne.setFirstName(userInput());
-		System.out.println("Please Enter Last Name : ");
-		personOne.setLastName(userInput());
-		System.out.println("Please Enter Address : ");
-		personOne.setAddress(userInput());
-		System.out.println("Please Enter City : ");
-		personOne.setCity(userInput());
-		System.out.println("Please Enter State : ");
-		personOne.setState(userInput());
-		System.out.println("Please Enter Zip Code : ");
-		personOne.setZipCode(userInput());
-		System.out.println("Please Enter Phone Number : ");
-		personOne.setPhoneNo(userInput());
-		System.out.println("Please Enter Email : ");
-		personOne.setEmail(userInput());
-		System.out.println(personOne);
-		
+		AddDetails addPersonDetail = new AddDetails();
+		menu = choice();
+		while (menu != 0) {
+			if (menu > 1)
+				menu = choice();
+			switch (menu) {
+			
+			case 1:
+				System.out.println("Enter First Name of contact which you would like to edit: ");
+				addPersonDetail.editContact(contact);
+				break;
+
+			default:
+				System.out.println("Please choose a valid Option");
+				break;
+
+			}
+			for (int i = 0; i < contact.size(); i++)
+				System.out.println(contact.get(i));
+			{
+				System.out.println("Goodbye!");
+			}
+		}
 	}
 }
-	
